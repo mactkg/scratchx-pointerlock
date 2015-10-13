168efc1e888ed3bc35445604543269c94d0fa21b
mactkg <mactkg@gmail.com>
2015-10-13 12:14:54 +0900

    init

--- scripts.js
+++ scripts.js
@@ -1,61 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports=// definition of blocks
 {
-  title: "scratchx-template",
+  title: "scratchx-pointerlock",
   descriptor: {
     en: {
       blocks: [
-        [' ', 'my first block: %n', 'myFirstBlock', 'val1']
+        [' ', 'Enable pointer lock', 'lock'],
+        [' ', 'Disable pointer lock', 'unlock'],
+        ['r', 'movement of mouse x', 'getMovementX'],
+        ['r', 'movement of mouse y', 'getMovementY'],
+        ['h', 'When pointer moved', 'whenMoved']
       ]
     },
     ja: {
       blocks: [
-        [' ', '最初のブロック: %n', 'myFirstBlock', '値1']
+        [' ', 'マウスロックを有効にする', 'lock'],
+        [' ', 'マウスロックを無効にする', 'unlock'],
+        ['r', 'x座標の移動量', 'getMovementX'],
+        ['r', 'y座標の移動量', 'getMovementY'],
+        ['h', 'マウスが動いたとき', 'whenMoved']
       ]
     }
   }
 }
 
 },{}],2:[function(require,module,exports){
-// write down codes here...
-module.exports = {
-  _shutdown: function() {
-  },
-
-  _getStatus: function() {
-    return {status: 2, msg: 'Ready'};
-  },
-
-  myFirstBlock: function(val) {
-    console.log("called myFirstBlock! val:", val);
-  }
-};
+module.exports={isMoved:!1,movementX:0,movementY:0,_shutdown:function(){},_getStatus:function(){return{status:2,msg:"Ready"}},lock:function(){document.body.reqPointerLock=document.body.requestPointerLock||document.body.mozRequestPointerLock||document.body.webkitRequestPointerLock,document.body.reqPointerLock();var e=this;document.addEventListener("mousemove",function(o){e.movementX=o.movementX||o.mozMovementX||o.webkitMovementX,e.movementY=o.movementY||o.mozMovementY||o.webkitMovementY,e.isMoved=!0})},unlock:function(){document.reqPointerUnlock=document.exitPointerLock||document.mozExitPointerLock||document.webkitExitPointerLock,document.reqPointerUnlock()},getMovementX:function(){var e=this.movementX;return e},getMovementY:function(){var e=this.movementY;return e},whenMoved:function(){var e=this.isMoved;return this.isMoved=!1,e}};
 
 },{}],3:[function(require,module,exports){
-var ext = require('./ext.js');
-var data = require('./data.json');
-
-(function(e) {
-  // Check for GET param 'lang'
-  // codes from https://github.com/khanning/scratch-arduino-extension/blob/da1ab317a215a8c1c5cda1b9db756b9edc14ba68/arduino_extension.js#L533-L541
-  var paramString = window.location.search.replace(/^\?|\/$/g, '');
-  var vars = paramString.split("&");
-  var lang = 'en';
-  for (var i=0; i<vars.length; i++) {
-    var pair = vars[i].split('=');
-    if (pair.length > 1 && pair[0]=='lang')
-      lang = pair[1];
-  }
-  
-  // merge objects
-  for (var attrname in ext) {
-    if (ext.hasOwnProperty(attrname)) {
-      e[attrname] = ext[attrname];
-    }
-  }
-
-  // register exention
-  ScratchExtensions.register(data.title, data.descriptor[lang], e);
-})({});
-
+var ext=require("./ext.js"),data=require("./data.json");!function(t){for(var e=window.location.search.replace(/^\?|\/$/g,""),r=e.split("&"),a="en",n=0;n<r.length;n++){var i=r[n].split("=");i.length>1&&"lang"==i[0]&&(a=i[1])}for(var o in ext)ext.hasOwnProperty(o)&&(t[o]=ext[o]);ScratchExtensions.register(data.title,data.descriptor[a],t)}({});
 },{"./data.json":1,"./ext.js":2}]},{},[3]);
--- src/ext.js
+++ src/ext.js
@@ -1,5 +1,9 @@
 // write down codes here...
 module.exports = {
+  isMoved: false,
+  movementX: 0,
+  movementY: 0,
+
   _shutdown: function() {
   },
 
@@ -7,7 +11,40 @@ module.exports = {
     return {status: 2, msg: 'Ready'};
   },
 
-  myFirstBlock: function(val) {
-    console.log("called myFirstBlock! val:", val);
+  lock: function() {
+    document.body.reqPointerLock =  document.body.requestPointerLock    ||
+                                    document.body.mozRequestPointerLock ||
+                                    document.body.webkitRequestPointerLock;
+    document.body.reqPointerLock();
+
+    var that = this;
+    document.addEventListener('mousemove', function(e) {
+      that.movementX = e.movementX || e.mozMovementX || e.webkitMovementX;
+      that.movementY = e.movementY || e.mozMovementY || e.webkitMovementY;
+      that.isMoved = true;
+    });
+  },
+
+  unlock: function() {
+    document.reqPointerUnlock = document.exitPointerLock    ||
+                                document.mozExitPointerLock ||
+                                document.webkitExitPointerLock;
+    document.reqPointerUnlock();
+  },
+
+  getMovementX: function() {
+    var val = this.movementX;
+    return val;
+  },
+
+  getMovementY: function() {
+    var val = this.movementY;
+    return val;
+  },
+
+  whenMoved: function() {
+    var val = this.isMoved;
+    this.isMoved = false;
+    return val;
   }
 };
